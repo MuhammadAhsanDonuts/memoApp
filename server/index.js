a883e27cd736a752e496891ea70c44fdc0de7172
Muhammad Ahsan <59800198+MuhammadAhsanDonuts@users.noreply.github.com>
2021-12-31 16:49:06 +0500

    Creating a Memo App

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from  'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose  from  'mongoose'
+import postRoutes  from './routes/posts.js'
+
+
+const app = express()
+
+
+app.use('/posts', postRoutes); 
+
+
+const PORT = process.env.PORT || 5000; 
+
+//setting up body-parser so we can send our requests to the server properly. 
+app.use(bodyParser.json({limit: "30mb", extended: true}))
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}))
+app.use(cors());
+
+//connecting database
+mongoose.connect('mongodb://localhost/27017', {useNewUrlParser: true }).then(()=> {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`)
+    })
+})
+.catch((error) => console.log(error.message)); 
+
+
+
+
